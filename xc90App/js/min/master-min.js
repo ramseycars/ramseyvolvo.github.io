9b4e8f5952e7eb9cc5737c086238dd6aec5d2b04
Joe <joethemovie@gmail.com>
2016-08-19 14:49:21 -0400

    Version 1.0 Created

---
+++ xc90App/js/min/master-min.js
@@ -0,0 +1,1 @@
+$(document).ready(function(){$(".honda").hide(),$(".acura").hide(),$("#bmw").hover(function(){$(".honda").hide(),$(".acura").hide(),$(".bmw").show()}),$("#honda").hover(function(){$(".bmw").hide(),$(".acura").hide(),$(".honda").show()}),$("#acura").hover(function(){$(".honda").hide(),$(".bmw").hide(),$(".acura").show()})});
